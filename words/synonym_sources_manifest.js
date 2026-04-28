ed09061290421f29c6e7debbe513a89cce72e980
lhp <2446268838@qq.com>
2026-04-28 22:37:02 +0800

    init

---
+++ words/synonym_sources_manifest.js
@@ -0,0 +1,12 @@
+window.SYNONYM_BUNDLE_META = {
+  "generatedAt": "2026-04-28T04:07:40.449413+00:00",
+  "totalSources": 6,
+  "totalGroups": 4063
+};
+window.SYNONYM_SOURCE_MAP = {};
+document.write('<script src="synonym_sources/source_001.js"><\/script>');
+document.write('<script src="synonym_sources/source_002.js"><\/script>');
+document.write('<script src="synonym_sources/source_003.js"><\/script>');
+document.write('<script src="synonym_sources/source_004.js"><\/script>');
+document.write('<script src="synonym_sources/source_005.js"><\/script>');
+document.write('<script src="synonym_sources/source_006.js"><\/script>');
